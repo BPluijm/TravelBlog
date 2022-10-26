76b74bb42d297bf40f109767ccfacfc9e92307e2
BPluijm <baspluijm@gmail.com>
2022-10-26 19:41:54 +0200

    finished about page

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -1,6 +1,7 @@
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import AboutImg from "../assets/australia.jpg";
+import Footer from "..//components/Footer";
 
 function Contact() {
     return(
@@ -12,6 +13,7 @@ function Contact() {
         title="Contact"
         btnClass="hide"
         />
+        <Footer />
         </>
     )
 }
