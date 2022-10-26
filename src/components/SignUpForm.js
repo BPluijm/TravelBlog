8adadf20ac634b22030a9584ef5b9ab7fd11bf9e
BPluijm <baspluijm@gmail.com>
2022-10-26 20:24:09 +0200

    added sign up page

---
+++ src/components/SignUpForm.js
@@ -0,0 +1,18 @@
+import "./SignUpFormStyles.css";
+
+function SignUpForm(){
+    return(
+        <div className="form-container">
+            <h1>Please Sign Up for more Amazing Trips!</h1>
+            <form>
+                <input placeholder="Userame"/>
+                <input placeholder="Email"/>
+                <input placeholder="Password"/>
+                <input placeholder="Repeat Password"/>
+                <button>Sign Up</button>
+            </form>
+        </div>
+    );
+}
+
+export default SignUpForm;
